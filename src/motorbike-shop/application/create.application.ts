ae5a99f387736d35f56891d14eb82e2ccbcda69d
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2024-08-24 13:06:57 -0600

    feat: added validation duplicate email

--- src/motorbike-shop/application/create.application.ts
+++ src/motorbike-shop/application/create.application.ts
@@ -4,6 +4,10 @@ export class CustomerCreateApplication {
   constructor(readonly repository: CustomerRepository) {}
 
   async create({name, email, phone, address}: Omit<CustomerDto, 'id' | 'credit'>): Promise<Pick<CustomerDto, 'id'>> {
+    const existeEmail = await this.repository.findByEmail(email);
+    if (existeEmail) {
+      throw new Error(`the email ${email} exist in database`)
+    }
     const customer = new CustomerDto(name, email, phone, address);
     return await this.repository.create(customer);
   }
--- src/motorbike-shop/domain/customer.repository.ts
+++ src/motorbike-shop/domain/customer.repository.ts
@@ -3,6 +3,7 @@ import { CustomerDto } from "./customer.dto";
 export interface CustomerRepository {
   create(customer: Omit<CustomerDto, 'id' | 'credit'>): Promise<Pick<CustomerDto, 'id'>>;
   findById(id: string): Promise<CustomerDto | null>;
+  findByEmail(email: string): Promise<boolean>;
   update(id: string, data: Omit<CustomerDto, 'id' | 'credit'>): Promise<void>;
   delete(id: string): Promise<void>;
 }
