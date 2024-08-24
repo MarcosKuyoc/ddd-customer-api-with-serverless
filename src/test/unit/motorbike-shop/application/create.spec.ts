c61c18fbe2cefd532ffc44d397ee68ca6f25112c
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2024-08-24 10:53:57 -0600

    test: add test for happy path an error occurred in database

---
+++ src/test/unit/motorbike-shop/application/create.spec.ts
@@ -0,0 +1,52 @@
+import { CreateApplication } from "../../../../motorbike-shop/application/create.application";
+import { CustomerDto } from "../../../../motorbike-shop/domain";
+
+describe('CreateApplication', () => {
+  const mockCustomerRepository = {
+    create: jest.fn(),
+    findById: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn()
+  };
+
+  const payload: Omit<CustomerDto, 'id' | 'credit'> = {
+    name: 'test-name',
+    email: 'test-email@gmail.com',
+    phone: 'phone-number',
+    address: 'address'
+  };
+
+  // happi path
+  it('should return the ID of created customer', async() => {
+    // Arrange
+    const customerId = {
+      id: '123456',
+    }
+    const mockCreate = mockCustomerRepository.create.mockResolvedValueOnce(customerId);
+
+    // Act
+    const customer = new CreateApplication(mockCustomerRepository);
+    const result = await customer.create(payload)
+    
+    // Asserts
+    expect(result).toBeDefined();
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+    expect(typeof result.id).toBe('string');
+  });
+
+  it('should return throw error in database', async() => {
+    // Arrange
+    const mockCreate = mockCustomerRepository.create.mockRejectedValueOnce(new Error('any error in database'));
+
+    try {
+      // Act
+      const customer = new CreateApplication(mockCustomerRepository);
+      await customer.create(payload);
+    } catch (error: any) {
+      // Asserts
+      expect(error).toBeDefined();
+      expect(mockCreate).toHaveBeenCalledTimes(1);
+      expect(error.message).toEqual('any error in database');
+    }
+  })
+})
