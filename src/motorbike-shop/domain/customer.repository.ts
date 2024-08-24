f64e316e0f040edcec95e7d2e02d23ebb089ea11
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2024-08-24 17:40:33 -0600

    feat: the dynamodb repository is modified, new methods are added and all functions are implemented

--- src/motorbike-shop/domain/customer.repository.ts
+++ src/motorbike-shop/domain/customer.repository.ts
@@ -2,8 +2,10 @@ import { CustomerDto } from "./customer.dto";
 
 export interface CustomerRepository {
   create(customer: Omit<CustomerDto, 'id' | 'credit'>): Promise<Pick<CustomerDto, 'id'>>;
+  find():Promise<CustomerDto[] | []>;
   findById(id: string): Promise<CustomerDto | null>;
   findByEmail(email: string): Promise<boolean>;
-  update(id: string, data: Omit<CustomerDto, 'id' | 'credit'>): Promise<void>;
+  sortedByCredit(): Promise<CustomerDto[]>;
+  update(id: string, data: any): Promise<void>;
   delete(id: string): Promise<void>;
 }
--- src/motorbike-shop/infraestructure/db/dynamodb.repository.ts
+++ src/motorbike-shop/infraestructure/db/dynamodb.repository.ts
@@ -10,7 +10,7 @@ export class DynamoDBRepository implements CustomerRepository {
       const id = uuidv4();
       const params = {
         TableName: TABLE_NAME,
-        Item: { ...customer, pk: id }
+        Item: { ...customer, id }
       }
       await DynamoDBClient.put(params).promise();
       return { id };
@@ -20,19 +20,46 @@ export class DynamoDBRepository implements CustomerRepository {
     }
   }
 
+  async find():Promise<CustomerDto[] | []> {
+    try {
+      const params = {
+        TableName: TABLE_NAME,
+        ProjectionExpression: '#id, #name, #email, #phone, #address, #credit',
+        ExpressionAttributeNames: {
+          '#id': 'id',
+          '#name': 'name',
+          '#email': 'email',
+          '#phone': 'phone',
+          '#address': 'address',
+          '#credit': 'credit',
+        }
+      }
+
+      const result = await DynamoDBClient.scan(params).promise();
+      if (result.Items) {
+        return result.Items as CustomerDto[];
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.error('DynamoDBRepository -> find');
+      throw error;
+    }
+  }
+
   async findById(id: string): Promise<CustomerDto | null> {
     try {
       const params = {
         TableName: TABLE_NAME,
-        KeyConditionExpression: 'pk = :pk',
-        ExpressionAttributeValues: { ':pk': id}
+        KeyConditionExpression: 'id = :id',
+        ExpressionAttributeValues: { ':id': id}
       }
       const customer = await DynamoDBClient.query(params).promise();
 
       if (customer.Items && customer.Items.length > 0) {
          const item = customer.Items[0];
          return {
-          id: item.pk,
+          id: item.id,
           name: item.name,
           email: item.email,
           phone: item.phone,
@@ -56,7 +83,7 @@ export class DynamoDBRepository implements CustomerRepository {
         KeyConditionExpression: 'email = :email',
         ExpressionAttributeValues: { ':email': email}
       };
-      console.log(params);
+
       const customer = await DynamoDBClient.query(params).promise();
 
       return (customer.Count ?? 0) > 0;
@@ -67,11 +94,86 @@ export class DynamoDBRepository implements CustomerRepository {
     }
   }
 
-  async update(id: string, data: Omit<CustomerDto, "id" | "credit">): Promise<void> {
-    throw new Error("Method not implemented.");
+  async sortedByCredit(): Promise<CustomerDto[] | []> {
+    try {
+      const params = {
+        TableName: TABLE_NAME,
+        IndexName: 'credit',
+        KeyConditionExpression: '#credit IS NOT NULL', // Filtra los ítems donde el atributo 'credit' no es nulo
+        FilterExpression: '#credit > :zero', // Filtra los ítems donde 'credit' es mayor que 0
+        ExpressionAttributeNames: {
+          '#id': 'id',
+          '#name': 'name',
+          '#email': 'email',
+          '#phone': 'phone',
+          '#address': 'address',
+          '#credit': 'credit'
+        },
+        ExpressionAttributeValues: {
+          ':zero': 0
+        },
+        ProjectionExpression: '#id, #name, #email, #phone, #address, #credit', // Usando los nombres escapados
+        ScanIndexForward: false, // Ordena en orden desendente
+      }
+
+      const result = await DynamoDBClient.scan(params).promise();
+      if (result.Items) {
+        return result.Items as CustomerDto[];
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.error('DynamoDBRepository -> sortedByCredit');
+      throw error;
+    }
   }
 
+  async update(id: string, data: any): Promise<void> {
+    try {
+      const ExpressionAttributeNames: { [key: string]: string } = {};
+      const ExpressionAttributeValues: { [key: string]: any } = {};
+      let UpdateExpression = 'SET';
+
+      // Genera nombres únicos para las claves de los atributos y valores
+      Object.entries(data).forEach(([attributeName, attributeValue], index) => {
+        const keyName = `#attr${index}`;
+        const valueName = `:val${index}`;
+
+        UpdateExpression += ` ${keyName} = ${valueName}, `;
+        ExpressionAttributeNames[keyName] = attributeName;
+        ExpressionAttributeValues[valueName] = attributeValue;
+      });
+
+      // Elimina la última coma y espacio de la expresión de actualización
+      UpdateExpression = UpdateExpression.slice(0, -2);
+
+      const params = {
+        TableName: TABLE_NAME,
+        Key: { id },
+        UpdateExpression,
+        ExpressionAttributeNames,
+        ExpressionAttributeValues,
+        ReturnValues: 'ALL_NEW' // Devuelve los nuevos valores del elemento después de la actualización
+      };
+
+      await DynamoDBClient.update(params).promise();
+    } catch (error) {
+      console.error('DynamoDBRepository -> update');
+      throw error;
+    }
+  }
+  
   async delete(id: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    try {
+      const params = {
+        TableName: TABLE_NAME,
+        Key: { id }
+      };
+
+      await DynamoDBClient.delete(params).promise();
+    } catch (error) {
+      console.error('DynamoDBRepository -> delete');
+      throw error;
+    }
   }
 }
